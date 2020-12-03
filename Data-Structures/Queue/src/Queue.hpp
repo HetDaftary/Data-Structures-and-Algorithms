5edbb9d7109244dd0a35c6c76c118f05c1baf774
HetDaftary <hetdaftary@gmail.com>
2020-12-03 16:56:36 +0530

    Data-Structures-and-Algorithms

--- Data-Structures/Queue/src/Queue.hpp
+++ Data-Structures/Queue/src/Queue.hpp
@@ -7,12 +7,24 @@ template<class T> class Queue {
         unsigned int front, rear, capacity;
     public:
         Queue() {
-
+            this -> arr = new T[1];
+            this -> front = 0;
+            this -> rear = 0;
+            this -> capacity = 1;
         }
 
         Queue(unsigned int initialCapacity) {
+            this -> arr = new T[1];
+            this -> front = 0;
+            this -> rear = 0;
+            this -> capacity = 1;
+        }
+
+        void enqueue() {
 
         }
 
-        
+        void dequeue() {
+
+        }
 };
