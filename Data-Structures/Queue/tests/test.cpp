92347a872f66781d5adf66450a803b7783bc0702
Het Daftary <51186433+HetDaftary@users.noreply.github.com>
2020-12-23 19:20:04 +0530

    Update test.cpp

--- Data-Structures/Queue/tests/test.cpp
+++ Data-Structures/Queue/tests/test.cpp
@@ -4,12 +4,12 @@ int main() {
     Queue<int> qu;
     for (int i = 0; i < 1000; i++) {
         qu.enqueue(i);
-        qu.displayQueue();
+        qu.displayQueue(cout);
     }
 
     while (!qu.isEmpty()) {
         qu.dequeue();
-        qu.displayQueue();
+        qu.displayQueue(cout);
     }
     return 0;
 }
