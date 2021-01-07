c2870be370f73e348eaa6b201d4c6b6bdeeacd5b
HetDaftary <hetdaftary@gmail.com>
2021-01-07 16:02:33 +0530

    Changing somethings

--- Algorithms/Sorting-Algorithms/Heap-Sort/includer.hpp
+++
@@ -1,5 +0,0 @@
-#if defined(_WIN32) || defined(_WIN64) || (defined(__CYGWIN__) && !defined(_WIN32)) // Windows uses a different system for path names. 
-    #include "src\HeapSort.hpp"
-#else 
-    #include "src/HeapSort.hpp"
-#endif
