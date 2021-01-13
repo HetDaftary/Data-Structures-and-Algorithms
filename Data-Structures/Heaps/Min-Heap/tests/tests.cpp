7becf4fe8991f2d1787f3cef6132b8cb7fab14ea
HetDaftary <hetdaftary@gmail.com>
2021-01-13 16:28:24 +0530

    Adding the test cases for Heap.

--- Data-Structures/Heaps/Min-Heap/tests/tests.cpp
+++ Data-Structures/Heaps/Min-Heap/tests/tests.cpp
@@ -19,5 +19,36 @@ int main() {
 
     minHeap.display(cout);
 
+    cout << "\n\nFixed size Heap" << endl;
+
+    // Fixed size heap.
+    MinHeap<int, true> m1(10);
+
+    for (int i = 0; i < 10; i++) {
+        m1.insert(i);
+    }
+
+    cout << m1.peek() << endl;
+
+    m1.display(cout);
+
+    cout << m1.deleteRoot() << endl;
+
+    m1.display(cout);
+
+    cout << "\n\nHeap using array" << endl;
+
+    // Make variable heap from array.
+    int arr[5] = {1, 2, 3, 0, 0};
+    MinHeap<int, false> m2(arr, 3);
+    // First three are the required elements.
+
+    cout << m2.peek() << endl;
+
+    m2.display(cout);
+
+    cout << m2.deleteRoot() << endl;
+
+    m2.display(cout);
     return 0;
 }
