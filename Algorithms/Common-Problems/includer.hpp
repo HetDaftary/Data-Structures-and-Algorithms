e5c2f9e1635b20b983688babfb9624c9bc7f8fe5
HetDaftary <hetdaftary@gmail.com>
2021-01-15 20:09:31 +0530

    Updating the structure

---
+++ Algorithms/Common-Problems/includer.hpp
@@ -0,0 +1,11 @@
+#if defined(_WIN32) || defined(_WIN64) || (defined(__CYGWIN__) && !defined(_WIN32)) // Windows uses a different system for path names. 
+    #include "Tower-Of-Hanoi\includer.hpp"
+    #include "Power-in-LogN\includer.hpp"
+    #include "Prime-Number-Test\includer.hpp"
+    #include "Fibonacci-In-Logn\includer.hpp"
+#else 
+    #include "Tower-Of-Hanoi/includer.hpp"
+    #include "Power-in-LogN/includer.hpp"
+    #include "Prime-Number-Test/includer.hpp"
+    #include "Fibonacci-In-Logn/includer.hpp"
+#endif
--- Algorithms/includer.hpp
+++ Algorithms/includer.hpp
@@ -1,13 +1,7 @@
 #if defined(_WIN32) || defined(_WIN64) || (defined(__CYGWIN__) && !defined(_WIN32)) // Windows uses a different system for path names. 
-    #include "Tower-Of-Hanoi\includer.hpp"
-    #include "Power-in-LogN\includer.hpp"
-    #include "Sorting-Algorithms/includer.hpp"
-    #include "Prime-Number-Test/includer.hpp"
-    #include "Fibonacci-In-Logn/includer.hpp"
+    #include "Sorting-Algorithms\includer.hpp"
+    #include "Common-Problems\includer.hpp"
 #else 
-    #include "Tower-Of-Hanoi/includer.hpp"
-    #include "Power-in-LogN/includer.hpp"
     #include "Sorting-Algorithms/includer.hpp"
-    #include "Prime-Number-Test/includer.hpp"
-    #include "Fibonacci-In-Logn/includer.hpp"
+    #include "Common-Problems/includer.hpp"
 #endif
